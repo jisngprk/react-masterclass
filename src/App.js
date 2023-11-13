bcd63ce0d051cb48a3c43c61764967a7dee9b0d5
jisng.park <jinsg.park@navercorp.com>
2023-11-13 23:02:21 +0900

    Adapting and Extending

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`
+
+const Circle = styled(Box)`
+  border-radius: 50px;
+`
+
+const Text = styled.span`
+
+`
+
 function App() {
-  return null;
+  return (
+      <Father>
+        <Box bgColor="teal">
+          <Text>Hello</Text>
+        </Box>
+        <Circle bgColor="tomato"></Circle>
+      </Father>
+
+  )
+
+
 }
 
 export default App;
