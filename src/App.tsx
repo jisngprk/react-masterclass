99f4e15a1c25ed22c431386884fc58156c75c7b0
jisng.park <jinsg.park@navercorp.com>
2023-11-16 01:49:13 +0900

    Form React event type

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import styled from "styled-components";
-import Circle from "./Circle";
+import React, {useState} from "react";
 
 function App() {
+    const [value, setValue] = useState("")
+    const onChange = (event: React.FormEvent<HTMLInputElement >) => {
+        const {
+            currentTarget: {value},
+        } = event;
+        console.log(value)
+        setValue(value);
+    }
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log("hello", value)
+    }
     return (
         <div>
-            <Circle bgColor={"teal"}/>
-            <Circle bgColor={"tomato"} borderColor={"black"} text={"Hello"}/>
+            <form onSubmit={onSubmit}>
+                <input value={value} onChange={onChange} type={"text"} placeholder={"username"}/>
+                <button>Log in</button>
+            </form>
         </div>
     );
 
---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import {DefaultTheme} from "styled-components";
+
+
+export const lightTheme:DefaultTheme = {
+    bgColor: 'whitesmoke',
+    textColor: 'black',
+    btnColor: 'tomato'
+}
+
+export const darkTheme:DefaultTheme = {
+    bgColor: 'black',
+    textColor: 'white',
+    btnColor: 'teal'
+}
